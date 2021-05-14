98cf5fbe127f193c77aaf7374fc9bda9f3520b74
AE-08 <69459186+kucukchef@users.noreply.github.com>
2021-05-14 21:35:26 +0300

    Create probability.py

---
+++ probability.py
@@ -0,0 +1,4 @@
+materials = int(input("Enter how many object/factor you have:"))
+mainMaterial = int(input("Enter count of what you want to find probability of:"))
+percentage = (100 / materials * mainMaterial)
+print("{}%".format(percentage))
